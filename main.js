3cee5764840ac2140cce8680c6093c311959c0d8
ElizabethOsorio1928 <55799665+ElizabethOsorio1928@users.noreply.github.com>
2021-11-28 21:17:34 -0600

    commit

---
+++ main.js
@@ -0,0 +1,206 @@
+const loggedOutLinks = document.querySelectorAll(".logged-out");
+const loggedInLinks = document.querySelectorAll(".logged-in");
+
+const loginCheck = (user) => {
+  if (user) {
+    loggedInLinks.forEach((link) => (link.style.display = "block"));
+    loggedOutLinks.forEach((link) => (link.style.display = "none"));
+  } else {
+    loggedInLinks.forEach((link) => (link.style.display = "none"));
+    loggedOutLinks.forEach((link) => (link.style.display = "block"));
+  }
+};
+
+// SignUp
+const signUpForm = document.querySelector("#signup-form");
+signUpForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = signUpForm["signup-email"].value;
+  const password = signUpForm["signup-password"].value;
+
+  // Authenticate the User
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // clear the form
+      signUpForm.reset();
+      // close the modal
+      $("#signupModal").modal("hide");
+    });
+});
+
+// Logout
+const logout = document.querySelector("#logout");
+
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log("signup out");
+  });
+});
+
+// SingIn
+const signInForm = document.querySelector("#login-form");
+
+signInForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = signInForm["login-email"].value;
+  const password = signInForm["login-password"].value;
+
+  // Authenticate the User
+  auth.signInWithEmailAndPassword(email, password).then((userCredential) => {
+    // clear the form
+    signInForm.reset();
+    // close the modal
+    $("#signinModal").modal("hide");
+  });
+});
+
+// Posts
+const postList = document.querySelector(".posts");
+const setupPosts = (data) => {
+  if (data.length) {
+    let html = "";
+    data.forEach((doc) => {
+      const post = doc.data();
+      const li = `
+      <li class="list-group-item list-group-item-action">
+        <h5>${post.title}</h5>
+        <p>${post.content}</p>
+      </li>
+    `;
+      html += li;
+    });
+    postList.innerHTML = html;
+  } else {
+    postList.innerHTML = '<h4 class="text-white">Lo que me gusta</h4>';
+  }
+};
+
+// events
+// list for auth state changes
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    console.log("signin");
+    fs.collection("posts")
+      .get()
+      .then((snapshot) => {
+        setupPosts(snapshot.docs);
+        loginCheck(user);
+      });
+  } else {
+    console.log("signout");
+    setupPosts([]);
+    loginCheck(user);
+  }
+});
+
+const db = firebase.firestore();
+
+const taskForm = document.getElementById("task-form");
+const tasksContainer = document.getElementById("tasks-container");
+
+let editStatus = false;
+let id = '';
+
+/**
+ * Save a New Task in Firestore
+ * @param {string} title the title of the Task
+ * @param {string} description the description of the Task
+ */
+const saveTask = (title, description) =>
+  db.collection("tasks").doc().set({
+    title,
+    description,
+  });
+
+const getTasks = () => db.collection("tasks").get();
+
+const onGetTasks = (callback) => db.collection("tasks").onSnapshot(callback);
+
+const deleteTask = (id) => db.collection("tasks").doc(id).delete();
+
+const getTask = (id) => db.collection("tasks").doc(id).get();
+
+const updateTask = (id, updatedTask) => db.collection('tasks').doc(id).update(updatedTask);
+
+window.addEventListener("DOMContentLoaded", async (e) => {
+  onGetTasks((querySnapshot) => {
+    tasksContainer.innerHTML = "";
+
+    querySnapshot.forEach((doc) => {
+      const task = doc.data();
+
+      tasksContainer.innerHTML += `<div class="card card-body mt-2 border-primary">
+    <h3 class="h5">${task.title}</h3>
+    <p>${task.description}</p>
+    <div>
+      <button class="btn btn-primary btn-delete" data-id="${doc.id}">
+        🗑 Delete
+      </button>
+      <button class="btn btn-secondary btn-edit" data-id="${doc.id}">
+        🖉 Edit
+      </button>
+    </div>
+  </div>`;
+    });
+
+    const btnsDelete = tasksContainer.querySelectorAll(".btn-delete");
+    btnsDelete.forEach((btn) =>
+      btn.addEventListener("click", async (e) => {
+        console.log(e.target.dataset.id);
+        try {
+          await deleteTask(e.target.dataset.id);
+        } catch (error) {
+          console.log(error);
+        }
+      })
+    );
+
+    const btnsEdit = tasksContainer.querySelectorAll(".btn-edit");
+    btnsEdit.forEach((btn) => {
+      btn.addEventListener("click", async (e) => {
+        try {
+          const doc = await getTask(e.target.dataset.id);
+          const task = doc.data();
+          taskForm["task-title"].value = task.title;
+          taskForm["task-description"].value = task.description;
+
+          editStatus = true;
+          id = doc.id;
+          taskForm["btn-task-form"].innerText = "Guardar";
+
+        } catch (error) {
+          console.log(error);
+        }
+      });
+    });
+  });
+});
+
+taskForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const title = taskForm["task-title"];
+  const description = taskForm["task-description"];
+
+  try {
+    if (!editStatus) {
+      await saveTask(title.value, description.value);
+    } else {
+      await updateTask(id, {
+        title: title.value,
+        description: description.value,
+      })
+
+      editStatus = false;
+      id = '';
+      taskForm['btn-task-form'].innerText = 'Guardar';
+    }
+
+    taskForm.reset();
+    title.focus();
+  } catch (error) {
+    console.log(error);
+  }
+});
